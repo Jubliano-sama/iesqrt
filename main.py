eca8c386c896b13171bc2ab35052088efad95e26
Tommie <tommie.kalb@gmail.com>
2021-01-29 14:30:46 +0100

    now supports keyboard input and always finds precise root

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-number = 523912
-iterations = 100
-neg = ""
-
-if number < 0:
-    number = -number
-    neg = "i"
-
-currentGuess = number
-iterativeAdd = currentGuess / 2
-
-for x in range(iterations):
-    if (currentGuess ** 2).__round__(9) < number:
-        currentGuess += iterativeAdd
-        iterativeAdd /= 2
-    elif (currentGuess ** 2).__round__(9) > number.__round__():
-        currentGuess -= iterativeAdd
-        iterativeAdd /= 2
-    else:
-        print("Found precise root!:")
-        print(currentGuess.__str__() + neg)
-        break
+def findRoot(number):
+    neg = ""
+    if number < 0:
+        number = -number
+        neg = "i"
+    currentGuess = number
+    iterativeAdd = currentGuess / 2
+    while (currentGuess ** 2).__round__(9) != number:
+        if (currentGuess ** 2).__round__(9) < number:
+            currentGuess += iterativeAdd
+            iterativeAdd /= 2
+        elif (currentGuess ** 2).__round__(9) > number.__round__():
+            currentGuess -= iterativeAdd
+            iterativeAdd /= 2
+    print("Found precise root!:")
     print(currentGuess.__str__() + neg)
+
+
+number = input("Enter a number to find the square root: ")
+while not number.isnumeric():
+    number = input("Please enter a valid number: ")
+
+findRoot(int(number))
