d7429a9f4049ead2460c0013b9aed82a0c1e2837
Tommie <tommie.kalb@gmail.com>
2021-01-29 14:05:01 +0100

    added check for when root is precise

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-number = 4
+number = 523912
 iterations = 100
+neg = ""
+
+if number < 0:
+    number = -number
+    neg = "i"
+
 currentGuess = number
 iterativeAdd = currentGuess / 2
 
 for x in range(iterations):
-    if currentGuess ** 2 < number:
+    if (currentGuess ** 2).__round__(9) < number:
         currentGuess += iterativeAdd
         iterativeAdd /= 2
-    elif currentGuess ** 2 > number:
+    elif (currentGuess ** 2).__round__(9) > number.__round__():
         currentGuess -= iterativeAdd
         iterativeAdd /= 2
-    print(currentGuess)
+    else:
+        print("Found precise root!:")
+        print(currentGuess.__str__() + neg)
+        break
+    print(currentGuess.__str__() + neg)
