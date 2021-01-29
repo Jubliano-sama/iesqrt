e60eb35743e8d6b49e33ef26c6b5b8c0deaad03d
Tommie <tommie.kalb@gmail.com>
2021-01-29 14:49:40 +0100

    now stops after 10000 iterations

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 def findRoot(number):
     neg = ""
+    iterations = 0
+    requiredDecimals = 10
     if number < 0:
         number = -number
         neg = "i"
     currentGuess = number
     iterativeAdd = currentGuess / 2
-    while (currentGuess ** 2).__round__(9) != number:
-        if (currentGuess ** 2).__round__(9) < number:
+    while (currentGuess ** 2).__round__(requiredDecimals) != number:
+        iterations += 1
+        if (currentGuess ** 2).__round__(requiredDecimals) < number:
             currentGuess += iterativeAdd
             iterativeAdd /= 2
-        elif (currentGuess ** 2).__round__(9) > number.__round__():
+        elif (currentGuess ** 2).__round__(requiredDecimals) > number.__round__():
             currentGuess -= iterativeAdd
             iterativeAdd /= 2
-    print("Found precise root!:")
-    print(currentGuess.__str__() + neg)
+        if iterations > 10000:
+            print(currentGuess)
+
+    print("Found precise root in %s iteration(s):" % iterations)
+    print(currentGuess.__round__(requiredDecimals).__str__() + neg)
 
 
 number = input("Enter a number to find the square root: ")
-while not number.isnumeric():
-    number = input("Please enter a valid number: ")
+while True:
+    try:
+        findRoot(int(number))
+        break
+    except(ValueError):
+        number = input("Please enter a valid number: ")
 
-findRoot(int(number))
+
